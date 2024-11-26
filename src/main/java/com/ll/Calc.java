136f4633f22e831502d89cba39b5808e9036d405
TTaiJin <taeparis980110@gmail.com>
2024-11-26 16:56:50 +0900

    work

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+public class Calc {
+
+    public static int run(String s) {
+        String[] sBits = s.split(" \\+ ");
+        int num1 = Integer.parseInt(sBits[0]);
+        int num2 = Integer.parseInt(sBits[1]);
+        return num1 + num2;
+    }
+}
