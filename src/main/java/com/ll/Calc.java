da63731c88fd7e65750ecb16a95ac19900c3e297
TTaiJin <taeparis980110@gmail.com>
2024-11-26 17:40:46 +0900

    work

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -13,6 +13,11 @@ public class Calc {
             int num1 = Integer.parseInt(sBits[0]);
             int num2 = Integer.parseInt(sBits[1]);
             return num1 * num2;
+        } else if (expr.contains(" / ")) {
+            String[] sBits = expr.split(" / ");
+            int num1 = Integer.parseInt(sBits[0]);
+            int num2 = Integer.parseInt(sBits[1]);
+            return num1 / num2;
         }
         return -1;
     }
